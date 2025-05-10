11ae6b3f344e57f4e27e1b3ac71b64512a4795e9
Anshuman Gupta <grario@Anshumans-MacBook-Air.local>
2025-05-10 09:44:16 +0530

    change f3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,19 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  define: {
+    "process.env": process.env,
+  },
+});
